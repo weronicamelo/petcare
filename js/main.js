e597f9a023ed46831e549a4c75e2e39e9c30e246
weronicamelo <weronicaamelo@gmail.com>
2025-05-27 10:51:01 -0300

    START, site PetCare: HTML, CSS e JS

--- js/main.js
+++ js/main.js
@@ -1,17 +1,24 @@
-// Selecione todos os elementos com a classe "duvida" 
+// Código do menu: destaque o link clicado
+const links = document.querySelectorAll('.menu nav a');
 
+links.forEach(link => {
+  link.addEventListener('click', function () {
+    links.forEach(l => l.classList.remove('selected'));
+    this.classList.add('selected');
+  });
+});
+
+// Código das dúvidas: abre uma, fecha as outras
 const elementosDuvida = document.querySelectorAll(".duvida");
 
 elementosDuvida.forEach(function (duvida) {
   duvida.addEventListener("click", function () {
-        // fecha as outras duvidas
-        elementosDuvida.forEach(function (outraDuvida) {
-            if (outraDuvida !== duvida) {
-              outraDuvida.classList.remove("ativa");
-            }
-        });
-
-        // abre a dúvida clicada
+    elementosDuvida.forEach(function (outraDuvida) {
+      if (outraDuvida !== duvida) {
+        outraDuvida.classList.remove("ativa");
+      }
+    });
+      //abre dúvida clicada
     duvida.classList.toggle("ativa");
   });
 });
